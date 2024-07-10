bb53c13a2a0e44e1f8dda88875b9b8242a03e4d3
taegyun <ypp1900@naver.com>
2024-07-10 16:58:35 +0900

    clip_grads 추가

--- common/util.py
+++ common/util.py
@@ -194,3 +194,14 @@ def eval_perplexity(model, corpus, batch_size=10, time_size=35):
     print('')
     ppl = np.exp(total_loss / max_iters)
     return ppl
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
